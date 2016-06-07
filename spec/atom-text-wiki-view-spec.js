83c4f7c56894f531e2e8c107017e289a725028b2
Wang Xu <wangxu@mail.dspread.com>
2016-06-07 23:43:27 +0800

    initial commit word count example

---
+++ spec/atom-text-wiki-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTextWikiView from '../lib/atom-text-wiki-view';
+
+describe('AtomTextWikiView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
